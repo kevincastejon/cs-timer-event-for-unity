0ac5d2c75f7a533a79de04d687c7841d3b9c35d0
Kevin Castejon <pitikev1@hotmail.com>
2020-02-29 00:38:35 +0100

    Initial commit

---
+++ Assets/TimerEventForUnity/Scripts/Example.cs
@@ -0,0 +1,31 @@
+﻿using kevincastejon;
+using UnityEngine;
+using Timer = kevincastejon.unity.Timer;
+
+namespace TimerEventExample
+{
+    public class Example : MonoBehaviour
+    {
+        public Timer Timer;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        public void OnTime(TimerEvent e)
+        {
+            Debug.Log(e.Name + " event has been dispatched");
+        }
+        public void OnComplete(TimerEvent e)
+        {
+            Debug.Log(e.Name + " event has been dispatched");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# cs-timer-event-for-unity
+ The TimerEvent package encapsulates the native System.Timers.Timer and the EventManager library together into a event-driven user friendly object.
